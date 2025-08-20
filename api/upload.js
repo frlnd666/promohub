5e17a7625e4dba3ae894996d4a7d2ad7f7909c2a
frlnd666 <74496050+frlnd666@users.noreply.github.com>
2025-08-20 10:21:46 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -2,20 +2,25 @@
 import { IncomingForm } from 'formidable';
 import cloudinary from 'cloudinary';
 
-// Konfigurasi Cloudinary dari variabel lingkungan
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// Fungsi untuk mem-parsing request file
+// Baris ini sangat penting
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const parseMultipart = (req) => {
   return new Promise((resolve, reject) => {
     const form = new IncomingForm();
     form.parse(req, (err, fields, files) => {
       if (err) return reject(err);
-      resolve(files.file[0]); // Perbaikan di sini, files.file bisa berupa array
+      resolve(files.file[0]);
     });
   });
 };
@@ -34,13 +39,11 @@ export default async function handler(req, res) {
       return;
     }
     
-    // Unggah file ke Cloudinary
     const result = await cloudinary.v2.uploader.upload(uploadedFile.filepath, {
       folder: 'promohub_invoices',
       resource_type: 'image'
     });
     
-    // Kirim respons dengan URL file yang berhasil diunggah
     res.status(200).json({
       message: 'File berhasil diunggah',
       url: result.secure_url
